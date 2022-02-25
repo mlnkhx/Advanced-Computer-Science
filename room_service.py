12221010683c65d563cb71a70b740e2f7d296312
Michelle Enkhjargal <98449219+mlnkhx@users.noreply.github.com>
2022-02-25 14:47:46 +0800

    Update room_service.py

--- room_service.py
+++ room_service.py
@@ -1,1 +1,10 @@
+#This is the list of guests in the hotel
+guests = {'123':'Zaya', '134':'Michelle', '204':'Jack'}
 
+room = input ("Please enter your room number: ")
+name = input ("Please enter your name: ")
+
+if guests[room].lower()==name.lower():
+  print ('Correct')
+else:
+  print ('Incorrect')
