9e0e007dc875a9a7f17dc25c22402c390d56f971
lerochkaaaa <98449106+lerochkaaaa@users.noreply.github.com>
2022-03-03 15:20:45 +0800

    Enkhzaya

--- room_service.py
+++ room_service.py
@@ -4,15 +4,32 @@ By Michelle And Enkhzaya
 """
 
 #This is the list of guests in the hotel
-room = input (“Please enter room number: “)
-if room in guests.keys():
-	Correct_info = True
-else:
-	Correct_info = False
+from cgi import print_form
+from tkinter import Place
 
-name = input (“Please enter your name:”)’
 
-if correct_info and guests[room].lower()==name.lower():
-	correct_info = True
-else:
-	correct_info = flase
+guests = {'102':'Ramirez', '105':'Ramirez', '107':'Ramirez'}
+starters = ['salad','paninni']
+
+Correct_info = False
+room = None
+name = None
+
+while not Correct_info:
+	room = input ("Please enter room number: ")
+	if room in guests.keys():
+		Correct_info = True
+	else:
+		Correct_info = False
+
+	name = input ("Please enter your name: ")
+
+	if Correct_info and guests[room].lower()==name.lower():
+		print ('Yepp')
+		Correct_info = True
+	else:
+		print ('Nope')
+		Correct_info = False
+
+	print ('Please, ')
+	print (starters)
